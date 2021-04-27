7fe5ad5e5acded048f148f15541ce39cf9715584
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-27 19:23:00 +0100

    Section 3 complete

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen"
+import ListScreen from "./src/screens/ListScreen"
+import ListScreenExercise from "./src/screens/ListScreenExercise"
 
 const navigator = createStackNavigator(
   {
     // Options for StackNavigator to show user
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    List: ListScreen,
+    ListExercise: ListScreenExercise
   },
   {
     // Initial is the name of the first route
-    initialRouteName: "Components",
+    initialRouteName: "ListExercise",
     defaultNavigationOptions: {
       title: "App",
     },
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,3 +1,49 @@
-import React from 'React';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const ListScreen = () => {
+  const friends = [
+    { name: "Friend #1" },
+    { name: "Friend #2" },
+    { name: "Friend #3" },
+    { name: "Friend #4" },
+    { name: "Friend #5" },
+    { name: "Friend #6" },
+    { name: "Friend #7" },
+    { name: "Friend #8" },
+    { name: "Friend #9" }
+  ]
+
+  // FlatList is better than mapping the array with RN.
+  // Keys must be unique and a string.
+  // can def in arry obj or keyextractor.
+
+  return (
+    <React.Fragment>
+    <Text>List Screen</Text>
+    <FlatList
+      // Scroll horizontally through the list.
+      horizontal
+      // Hide the scroll bar on the bottom
+      showsHorizontalScrollIndicator={false}
+      
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        // element === { item: {name:friend...}, index: 0 }
+        return <Text style={styles.textStyle}>{item.name}</Text>;
+      }}
+    />
+    </React.Fragment>
+
+  ); 
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
+
+export default ListScreen;
 
