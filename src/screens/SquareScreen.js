b122b9562608bbabef02c5c20b41e83c198944d7
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-28 19:51:05 +0100

    reducers

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,58 +1,61 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, StyleSheet, Button, Text } from 'react-native';
 import ColourCounter from "../components/ColourCounter"
 
 const COLOUR_INCREMENT = 15;
 
-const SquareScreen = () => {
-  const[red, setRed] = useState(0);
-  const[green, setGreen] = useState(0);
-  const[blue, setBlue] = useState(0);
+// defined outside so not mixed up with state within SquareScreen - convention.
+// action - how to change state object
+const reducer = (state, action) => {
+  // state === { red: number, green: number, blue: number };
+  // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: +-15}
+  switch (action.type) {
+    case 'change_red':
+      return state.red + action.payload > 255 || state.red + action.payload < 0
+        ? state 
+        : {...state, red: state.red + action.payload };
+    case 'change_green':
+      return state.green + action.payload > 255 || state.green + action.payload < 0
+        ? state 
+        : {...state, green: state.green + action.payload };
+    case 'change_blue':
+      return state.blue + action.payload > 255 || state.blue + action.payload < 0
+        ? state 
+        : {...state, blue: state.blue + action.payload };
+    default:
+      return state;
+  }
+  // In every scenario we return a state.
+};
 
-  // Polymorphic version of the bodged way below.
-  // Works out quite nicely as helper method is a lot clearer + abstracts the logic
-  const setColour = (colour, change) => {
-    // colour === 'red', 'green', 'blue'
-    // change === +- 15
+const SquareScreen = () => { 
 
-    switch (colour) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-        return;
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-        return;
-      default:
-        return;
-    }
-  };
+  // state is initally { red: 0, green: 0, blue: 0 }
+  // dispatch runs the reducer with an action parameter
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
   return (
     <View>
         <ColourCounter 
-          onIncrease={() => setColour('red', COLOUR_INCREMENT)}
-          onDecrease={() => setColour('red', -COLOUR_INCREMENT)}
+          onIncrease={() => dispatch({ type: 'change_red', payload: COLOUR_INCREMENT })}
+          onDecrease={() => dispatch({ type: 'change_red', payload: -1 * COLOUR_INCREMENT })}
           colour="Red"
-          
-          />
+        />
         <ColourCounter 
           colour="Green"
-          onIncrease={() => setColour('green', COLOUR_INCREMENT)}
-          onDecrease={() => setColour('green', -COLOUR_INCREMENT)}
+          onIncrease={() => dispatch({ type: 'change_green', payload: COLOUR_INCREMENT })}
+          onDecrease={() => dispatch({ type: 'change_green', payload: -1 * COLOUR_INCREMENT })}
         />
         <ColourCounter 
           colour="Blue"
-          onIncrease={() => setColour('blue', COLOUR_INCREMENT)}
-          onDecrease={() => setColour('blue', -COLOUR_INCREMENT)}
+          onIncrease={() => dispatch({ type: 'change_blue', payload: COLOUR_INCREMENT })}
+          onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * COLOUR_INCREMENT })}
         />
         <View 
           style={{ 
             height: 200, 
             width: 200, 
-            backgroundColor: `rgb(${red},${green},${blue})` 
+            backgroundColor: `rgb(${state.red},${state.green},${state.blue})` 
             }} 
         />
     </View>
@@ -84,4 +87,33 @@ onDecrease={() => {
 }}
 colour="Red"
 />
+*/
+
+// Before reducer, using useState hook
+
+/*
+const[red, setRed] = useState(0);
+  const[green, setGreen] = useState(0);
+  const[blue, setBlue] = useState(0);
+
+  // Polymorphic version of the bodged way below.
+  // Works out quite nicely as helper method is a lot clearer + abstracts the logic
+  const setColour = (colour, change) => {
+    // colour === 'red', 'green', 'blue'
+    // change === +- 15
+
+    switch (colour) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
+      default:
+        return;
+    }
+  };
 */
